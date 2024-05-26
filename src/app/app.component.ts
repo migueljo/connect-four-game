aeaa8ccdd805b26e4bf4c039fa1603a421de3134
Miguel Martínez <migueljomartinez@gmail.com>
2024-05-26 12:37:46 -0500

    Adding main-menu, in-game, and rules to the router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { MainMenuComponent } from './main-menu/main-menu.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MainMenuComponent, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'connect-four-game';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { MainMenuComponent } from './main-menu/main-menu.component';
+import { InGameComponent } from './in-game/in-game.component';
+import { RulesComponent } from './rules/rules.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainMenuComponent,
+  },
+  {
+    path: 'in-game',
+    component: InGameComponent,
+  },
+  {
+    path: 'rules',
+    component: RulesComponent,
+  },
+];
